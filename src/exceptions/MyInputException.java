dc228d3f9a807df677e6f74621290dfe75d61ad3
NicMcPhee <mcphee@morris.umn.edu>
2014-01-15 16:48:21 -0600

    Added a serialization UID to MyInputException and cleaned up the formatting for that class.

--- src/exceptions/MyInputException.java
+++ src/exceptions/MyInputException.java
@@ -1,17 +1,19 @@
-
 /** This exception is thrown when an input
-  * exception occurs.  It is used by MyInputStream
-  * to convert checked exceptions such as
-  * FileNotFoundException and EOFException
-  * into unchecked runtime exceptions. */
+ * exception occurs.  It is used by MyInputStream
+ * to convert checked exceptions such as
+ * FileNotFoundException and EOFException
+ * into unchecked runtime exceptions. */
 
 package exceptions;
 
-public class MyInputException
-       extends RuntimeException
-{
-   public MyInputException()
-      {super();}
-   public MyInputException(String message)
-      {super(message);}
+public class MyInputException extends RuntimeException {
+	private static final long serialVersionUID = -7921978290082262832L;
+
+	public MyInputException() {
+		super();
+	}
+
+	public MyInputException(String message) {
+		super(message);
+	}
 }
