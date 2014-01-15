7255234ccd26392929e4f2337f028bf94feeb784
NicMcPhee <mcphee@morris.umn.edu>
2014-01-15 16:55:02 -0600

    Cleaned up the formatting throughout the code base.

--- src/dataStructures/ChainNode.java
+++ src/dataStructures/ChainNode.java
@@ -1,24 +1,24 @@
-
 /** Node class used by linked structures.
-  * This class and its data members are
-  * visible only within the package dataStructures. */
+ * This class and its data members are
+ * visible only within the package dataStructures. */
 
 package dataStructures;
 
+class ChainNode {
+	// package visible data members
+	Object element;
+	ChainNode next;
 
-class ChainNode
-{
-   // package visible data members
-   Object element;
-   ChainNode next;
+	// package visible constructors
+	ChainNode() {
+	}
 
-   // package visible constructors
-   ChainNode() {}
-     
-   ChainNode(Object element)
-      {this.element = element;}
+	ChainNode(Object element) {
+		this.element = element;
+	}
 
-   ChainNode(Object element, ChainNode next)
-      {this.element = element;
-       this.next = next;}
+	ChainNode(Object element, ChainNode next) {
+		this.element = element;
+		this.next = next;
+	}
 }
--- src/utilities/MyInputStream.java
+++ src/utilities/MyInputStream.java
@@ -11,193 +11,175 @@ import java.util.StringTokenizer;
 
 import exceptions.MyInputException;
 
-public class MyInputStream
-{
-   // data members
-   private BufferedReader in;
-   private StringTokenizer line;
+public class MyInputStream {
+	// data members
+	private BufferedReader in;
+	private StringTokenizer line;
 
-   /** Constructor to set up BufferedReader
-     * for standard input stream System.in.
-     * Typically, this is the keyboard. */
-    
-   public MyInputStream()
-   {
-      in = new BufferedReader
-               (new InputStreamReader(System.in));
-   }
+	/**
+	 * Constructor to set up BufferedReader for standard input stream System.in.
+	 * Typically, this is the keyboard.
+	 */
 
-   /** Constructor to set up BufferedReader
-     * for input from a file. */
-     
-   public MyInputStream(String fileName)
-   {
-      try
-      {
-         in = new BufferedReader
-                  (new FileReader(fileName));
-      }
-      catch (FileNotFoundException e)
-         {throw new MyInputException(e.getMessage());}
-   }
-         
-   /** Discard remainder of current line. */
-   public void flushCurrentLine()
-   {
-      line = null;
-   }
-         
-   /** Extract next token from input stream.
-     * @return next token in input stream
-     * @exception MyInputException thrown if
-     * some error occurs while inputting a new
-     * line or if the end of file is reached */
-     
-   private String nextToken()
-   {
-      // ensure that the current line has a token
-      while (line == null || !line.hasMoreTokens())
-      {
-         try
-         {
-            // attempt to input another line
-            String newLine = in.readLine();
-            if (newLine == null)  // end of file encountered
-               throw new MyInputException("End of file");
+	public MyInputStream() {
+		in = new BufferedReader(new InputStreamReader(System.in));
+	}
 
-            // convert newLine to tokens
-            line = new StringTokenizer(newLine);
-         }
-         catch (IOException e)
-            {throw new MyInputException(e.getMessage());}
+	/**
+	 * Constructor to set up BufferedReader for input from a file.
+	 */
 
-      }
+	public MyInputStream(String fileName) {
+		try {
+			in = new BufferedReader(new FileReader(fileName));
+		} catch (FileNotFoundException e) {
+			throw new MyInputException(e.getMessage());
+		}
+	}
 
-      // extract and return the next token
-      return line.nextToken();
-   }
+	/** Discard remainder of current line. */
+	public void flushCurrentLine() {
+		line = null;
+	}
 
-   /** Input an integer.
-     * @exception MyInputException
-     * @exception NumberFormatException
-     * @return next token as an integer number */
-     
-   public int readInteger()
-   {
-      return Integer.parseInt(nextToken());
-   }
+	/**
+	 * Extract next token from input stream.
+	 * 
+	 * @return next token in input stream
+	 * @exception MyInputException
+	 *                thrown if some error occurs while inputting a new line or
+	 *                if the end of file is reached
+	 */
 
-   /** Input a long. */
-   public long readLong()
-   {
-      return Long.parseLong(nextToken());
-   }
+	private String nextToken() {
+		// ensure that the current line has a token
+		while (line == null || !line.hasMoreTokens()) {
+			try {
+				// attempt to input another line
+				String newLine = in.readLine();
+				if (newLine == null) // end of file encountered
+					throw new MyInputException("End of file");
 
-   /** Input a short. */
-   public short readShort()
-   {
-      return Short.parseShort(nextToken());
-   }
+				// convert newLine to tokens
+				line = new StringTokenizer(newLine);
+			} catch (IOException e) {
+				throw new MyInputException(e.getMessage());
+			}
 
-   /** Input a byte. */
-   public byte readByte()
-   {
-      return Byte.parseByte(nextToken());
-   }
+		}
 
-   /** Input a float. */
-   public float readFloat()
-   {
-      return new Float(nextToken()).floatValue();
-   }
+		// extract and return the next token
+		return line.nextToken();
+	}
 
-   /** Input a double. */
-   public double readDouble()
-   {
-      return new Double(nextToken()).doubleValue();
-   }
+	/**
+	 * Input an integer.
+	 * 
+	 * @exception MyInputException
+	 * @exception NumberFormatException
+	 * @return next token as an integer number
+	 */
 
-   /** Input a string. */
-   public String readString()
-   {
-      return nextToken();
-   }
+	public int readInteger() {
+		return Integer.parseInt(nextToken());
+	}
 
-   /** Input a boolean. */
-   public boolean readBoolean()
-   {
-      String b = nextToken();
-      if (b.equals("true")) return true;
-      if (b.equals("false")) return false;
-      throw new MyInputException
-                ("Input is not a boolean");
-   }
+	/** Input a long. */
+	public long readLong() {
+		return Long.parseLong(nextToken());
+	}
 
-   /** test program */
-   public static void main(String [] args)
-   {
-      boolean b;
-      int i, j, k;
-      double d;
-      byte c;
-      short f;
-      String g;
+	/** Input a short. */
+	public short readShort() {
+		return Short.parseShort(nextToken());
+	}
 
-      // define a MyInputStream object to input
-      // from System.in and another to input from
-      // the file utilities\MyInputStream.file
-      MyInputStream keyboard = new MyInputStream(),
-                    file = new MyInputStream
-                           ("utilities/MyInputStream.file");
+	/** Input a byte. */
+	public byte readByte() {
+		return Byte.parseByte(nextToken());
+	}
 
-      System.out.println("Enter a boolean, an integer " +
-                         "a double, a byte, a short, " +
-                         " and a string");
-      System.out.flush();  // force output of last line
-   
-      b = keyboard.readBoolean();
-      i = keyboard.readInteger();
-      d = keyboard.readDouble();
-      c = keyboard.readByte();
-      f = keyboard.readShort();
-      g = keyboard.readString();
-      keyboard.flushCurrentLine();
-      
-      System.out.println("The input is " + b +
-                         " " + i + " " + d + " " +
-                         c + " " + f + " " + g);
+	/** Input a float. */
+	public float readFloat() {
+		return new Float(nextToken()).floatValue();
+	}
 
-      // now read from the file and output
-      b = file.readBoolean();
-      i = file.readInteger();
-      d = file.readDouble();
-      keyboard.flushCurrentLine();
-      
-      System.out.println("The file input is " + b +
-                         " " + i + " " + d);
+	/** Input a double. */
+	public double readDouble() {
+		return new Double(nextToken()).doubleValue();
+	}
 
-      System.out.println("Enter three integers, " +
-                         "one to a line");
-      System.out.flush();  // force output of last line
-      i = keyboard.readInteger();
-      keyboard.flushCurrentLine();
-      j = keyboard.readInteger();
-      keyboard.flushCurrentLine();
-      k = keyboard.readInteger();
-      keyboard.flushCurrentLine();
-      
-      System.out.println("The input is " + i +
-                         " " + j + " " + k);
+	/** Input a string. */
+	public String readString() {
+		return nextToken();
+	}
 
-      // try another read from the file
-      // should throw end of file exception
-      file.flushCurrentLine();
-      try {
-         b = file.readBoolean();
-         }
-      catch (MyInputException e)
-         {System.out.println
-          ("MyInputException: " + e.getMessage());
-         }
-   }
+	/** Input a boolean. */
+	public boolean readBoolean() {
+		String b = nextToken();
+		if (b.equals("true"))
+			return true;
+		if (b.equals("false"))
+			return false;
+		throw new MyInputException("Input is not a boolean");
+	}
+
+	/** test program */
+	public static void main(String[] args) {
+		boolean b;
+		int i, j, k;
+		double d;
+		byte c;
+		short f;
+		String g;
+
+		// define a MyInputStream object to input
+		// from System.in and another to input from
+		// the file utilities\MyInputStream.file
+		MyInputStream keyboard = new MyInputStream(), file = new MyInputStream(
+				"utilities/MyInputStream.file");
+
+		System.out.println("Enter a boolean, an integer "
+				+ "a double, a byte, a short, " + " and a string");
+		System.out.flush(); // force output of last line
+
+		b = keyboard.readBoolean();
+		i = keyboard.readInteger();
+		d = keyboard.readDouble();
+		c = keyboard.readByte();
+		f = keyboard.readShort();
+		g = keyboard.readString();
+		keyboard.flushCurrentLine();
+
+		System.out.println("The input is " + b + " " + i + " " + d + " " + c
+				+ " " + f + " " + g);
+
+		// now read from the file and output
+		b = file.readBoolean();
+		i = file.readInteger();
+		d = file.readDouble();
+		keyboard.flushCurrentLine();
+
+		System.out.println("The file input is " + b + " " + i + " " + d);
+
+		System.out.println("Enter three integers, " + "one to a line");
+		System.out.flush(); // force output of last line
+		i = keyboard.readInteger();
+		keyboard.flushCurrentLine();
+		j = keyboard.readInteger();
+		keyboard.flushCurrentLine();
+		k = keyboard.readInteger();
+		keyboard.flushCurrentLine();
+
+		System.out.println("The input is " + i + " " + j + " " + k);
+
+		// try another read from the file
+		// should throw end of file exception
+		file.flushCurrentLine();
+		try {
+			b = file.readBoolean();
+		} catch (MyInputException e) {
+			System.out.println("MyInputException: " + e.getMessage());
+		}
+	}
 }
