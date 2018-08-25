cf8358da4bb49f2bc6c9d1d937f49da31c6b5043
Nic McPhee <mcphee@morris.umn.edu>
2018-08-25 11:32:31 -0500

    Extract input reading and parsing into its own class
    The `MachineShopSimulator` doesn't need the input and parsing logic
    cluttering up the simulator logic, so this extracts that into a new
    class.
    
    Everything there is `static` at the moment, and I would like to fix
    that.

--- src/applications/MachineShopSimulator.java
+++ src/applications/MachineShopSimulator.java
@@ -2,9 +2,6 @@
 
 package applications;
 
-import utilities.MyInputStream;
-import exceptions.MyInputException;
-
 public class MachineShopSimulator {
     
     public static final String NUMBER_OF_MACHINES_MUST_BE_AT_LEAST_1 = "number of machines must be >= 1";
@@ -80,68 +77,9 @@ public class MachineShopSimulator {
         return lastJob;
     }
 
-    /** input machine shop data */
-    static SimulationSpecification inputData() {
-        SimulationSpecification specification = new SimulationSpecification();
-
-        // define the input stream to be the standard input stream
-        MyInputStream keyboard = new MyInputStream();
-
-        readNumberMachinesAndJobs(specification, keyboard);
-
-        readChangeOverTimes(specification, keyboard);
-
-        readJobSpecifications(specification, keyboard);
-
-        return specification;
-    }
-
     private static void setMachineChangeOverTimes(SimulationSpecification specification) {
         for (int i = 1; i<=specification.getNumMachines(); ++i) {
             machine[i].setChangeTime(specification.getChangeOverTimes(i));
-        }
-    }
-
-    private static void readNumberMachinesAndJobs(SimulationSpecification specification, MyInputStream keyboard) {
-        System.out.println("Enter number of machines and jobs");
-        int numMachines = keyboard.readInteger();
-        int numJobs = keyboard.readInteger();
-        if (numMachines < 1 || numJobs < 1) {
-            throw new MyInputException(NUMBER_OF_MACHINES_AND_JOBS_MUST_BE_AT_LEAST_1);
-        } else {
-            specification.setNumMachines(numMachines);
-            specification.setNumJobs(numJobs);
-        }
-    }
-
-    private static void readJobSpecifications(SimulationSpecification specification, MyInputStream keyboard) {
-        // input the jobs
-        JobSpecification[] jobSpecifications = new JobSpecification[specification.getNumJobs()+1];
-        for (int i=1; i <= specification.getNumJobs(); i++) {
-            jobSpecifications[i] = new JobSpecification();
-        }
-        specification.setJobSpecification(jobSpecifications);
-        for (int i = 1; i <= specification.getNumJobs(); i++) {
-            System.out.println("Enter number of tasks for job " + i);
-            int tasks = keyboard.readInteger(); // number of tasks
-            if (tasks < 1)
-                throw new MyInputException(EACH_JOB_MUST_HAVE_AT_LEAST_1_TASK);
-            jobSpecifications[i].setNumTasks(tasks);
-
-            int[] specificationsForTasks = new int[2 * tasks + 1];
-
-            System.out.println("Enter the tasks (machine, time)"
-                    + " in process order");
-            for (int j = 1; j <= tasks; j++) { // get tasks for job i
-                int theMachine = keyboard.readInteger();
-                int theTaskTime = keyboard.readInteger();
-                if (theMachine < 1 || theMachine > specification.getNumMachines()
-                        || theTaskTime < 1)
-                    throw new MyInputException(BAD_MACHINE_NUMBER_OR_TASK_TIME);
-                specificationsForTasks[2*(j-1)+1] = theMachine;
-                specificationsForTasks[2*(j-1)+2] = theTaskTime;
-            }
-            specification.setSpecificationsForTasks(i, specificationsForTasks);
         }
     }
 
@@ -163,21 +101,6 @@ public class MachineShopSimulator {
             } // task queue
             machine[firstMachine].getJobQ().put(theJob);
         }
-    }
-
-    private static void readChangeOverTimes(SimulationSpecification specification, MyInputStream keyboard) {
-        // input the change-over times
-        int changeOverTimes[] = new int[specification.getNumMachines()+1];
-
-        System.out.println("Enter change-over times for machines");
-        for (int j = 1; j <= specification.getNumMachines(); j++) {
-            int ct = keyboard.readInteger();
-            if (ct < 0)
-                throw new MyInputException(CHANGE_OVER_TIME_MUST_BE_AT_LEAST_0);
-            changeOverTimes[j] = ct;
-        }
-
-        specification.setChangeOverTimes(changeOverTimes);
     }
 
     private static void createEventAndMachineQueues(SimulationSpecification specification) {
@@ -243,7 +166,7 @@ public class MachineShopSimulator {
          * not convinced this is the best place for this to happen, though.
          */
         timeNow = 0;
-        SimulationSpecification specification = inputData(); // get machine and job data
+        SimulationSpecification specification = SpecificationReader.readSpecification(); // get machine and job data
         startShop(specification); // initial machine loading
         simulate(); // run all jobs through shop
         SimulationResults simulationResults = outputStatistics(); // output machine wait times
